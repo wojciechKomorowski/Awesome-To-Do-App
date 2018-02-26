6e4da684865c752fb932a78d9151a09a356bf151
Wojciech Komorowski <komora89@gmail.com>
2018-02-26 14:46:11 +0100

    Index file for loading routes.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Core\Router;
+use App\Core\Request;
+
+require 'vendor/autoload.php';
+
+require 'core/bootstrap.php';
+
+Router::load('app/routes.php')
+    ->direct(Request::uri(), Request::method());
