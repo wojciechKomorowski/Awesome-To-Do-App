ac06857663dcb3ed3bac67aee162a65d64d34762
Wojciech Komorowski <komora89@gmail.com>
2018-02-26 14:53:59 +0100

    Main view file.

---
+++ app/views/index.view.php
@@ -0,0 +1,11 @@
+<?php 
+
+require 'app/views/partials/header.php';
+
+require 'app/views/partials/master.php';
+
+require 'app/views/partials/task-list.php';
+
+require 'app/views/partials/new-task.php';
+
+require 'app/views/partials/footer.php'; 
